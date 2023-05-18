bf8d0cbee66633aa5b84228f2f4c355615de10e7
jicking bebiro <7297029+jicking@users.noreply.github.com>
2023-05-18 17:07:38 +0800

    set main layout's ssr=false to make app an SPA

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	// import Header from '../lib/Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="page-root app">
+	<!-- <Header /> -->
+	<main>
+		<slot />
+	</main>
+	<footer><!-- footer --></footer>
+</div>
+
+<style>
+    /*  */
+</style>
