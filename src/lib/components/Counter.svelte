9a5766ee1f9cb99b23f5b99730e514aa5e0a7286
jicking bebiro <7297029+jicking@users.noreply.github.com>
2023-05-18 18:00:50 +0800

    set counter component

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	// @todo use $lib module - https://kit.svelte.dev/docs/modules
+	import { count } from "../shared/store";
+</script>
+
+<div class="counter">
+    <h1>Counter : {$count}</h1>
+	<button on:click={count.increment} class="counter-btn">
+		+
+	</button>
+    <button on:click={count.decrement} class="counter-btn">
+		-
+	</button>
+    <button on:click={() => $count *= $count} class="counter-btn">
+		power
+	</button>
+    <button on:click={count.reset} class="counter-btn">
+		reset
+	</button>
+</div>
+
+<style>
+	.counter {
+		margin: 8px 0;
+	}
+	.counter-btn {
+		padding: 8px;
+	}
+</style>
